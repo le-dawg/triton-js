77aa310dcaa15b180e3ddcf522f3a39bd436b374
dawg <dawid.golebiewski@tuhh.de>
2022-12-09 11:30:52 +0100

    Create klaviyo.js

---
+++ klaviyo.js
@@ -0,0 +1,4 @@
+document.querySelector('.klaviyo_form_trigger').addEventListener('click', function (){
+  window._klOnsite = window._klOnsite || []; 
+	window._klOnsite.push(['openForm', 'T9wkbS']);
+  });
